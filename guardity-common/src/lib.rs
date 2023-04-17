33acc1fe59ca99245180dab11b16aea123cc5175
Michal Rostecki <vadorovsky@protonmail.com>
2023-04-17 19:26:18 +0200

    Add policy violation alerts
    Output violation alerts via perf buffers.

--- guardity-common/src/lib.rs
+++ guardity-common/src/lib.rs
@@ -3,6 +3,27 @@
 pub const MAX_PATHS: usize = 8;
 
 #[repr(C)]
+#[cfg_attr(feature = "user", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Copy, Clone)]
+pub struct FileOpenAlert {
+    pub pid: u64,
+    pub binprm_inode: u64,
+    pub inode: u64,
+}
+
+#[repr(C)]
+#[cfg_attr(feature = "user", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Copy, Clone)]
+pub struct SetuidAlert {
+    pub pid: u64,
+    pub binprm_inode: u64,
+    pub old_uid: u32,
+    pub old_gid: u32,
+    pub new_uid: u32,
+    pub new_gid: u32,
+}
+
+#[repr(C)]
 #[derive(Copy, Clone)]
 pub struct Paths {
     pub all: bool,
@@ -15,5 +36,9 @@ pub struct Paths {
 pub mod user {
     use super::*;
 
-    unsafe impl aya::Pod for Paths {}
+    use aya::Pod;
+
+    unsafe impl Pod for FileOpenAlert {}
+    unsafe impl Pod for SetuidAlert {}
+    unsafe impl Pod for Paths {}
 }
