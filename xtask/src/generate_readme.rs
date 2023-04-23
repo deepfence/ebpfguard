d9307074dec3b731d0f3c793e25ea2c74dc2d9af
Michal Rostecki <vadorovsky@protonmail.com>
2023-04-23 15:19:37 +0200

    Fix doc tests, autogenerate README

---
+++ xtask/src/generate_readme.rs
@@ -0,0 +1,38 @@
+use std::{env::current_dir, fs::File};
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub struct Options {
+    /// Do not overwrite the README.md file, just check whether it is up to date.
+    #[clap(long, default_value_t = false)]
+    check: bool,
+}
+
+pub fn generate_readme(opts: Options) -> anyhow::Result<()> {
+    let project_root = current_dir()?.join("guardity");
+    let mut source = File::open("guardity/src/lib.rs")?;
+    let mut template = File::open("README.tpl")?;
+
+    let content = cargo_readme::generate_readme(
+        project_root.as_path(),
+        &mut source,
+        Some(&mut template),
+        true,
+        true,
+        true,
+        true,
+    )
+    .map_err(|e| anyhow::anyhow!(e))?;
+
+    if opts.check {
+        let readme = std::fs::read_to_string("README.md")?;
+        if readme != content {
+            anyhow::bail!("README.md is not up to date");
+        }
+    } else {
+        std::fs::write("README.md", content)?;
+    }
+
+    Ok(())
+}
